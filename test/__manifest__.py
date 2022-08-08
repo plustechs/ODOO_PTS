0d4d00153fe83715d30422127c26cd1e72ccbaa3
Jean Carlo <jeancarlobenites@gmail.com>
2022-08-08 18:03:15 -0500

    manifest

--- test/__manifest__.py
+++ test/__manifest__.py
@@ -3,21 +3,21 @@
     'name': "test",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Test module for Odoo
+        """,
 
     'description': """
-        Long description of module's purpose
+        Test module for Odoo
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Plustechs",
+    'website': "http://www.plustechs.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Productivity',
+    'version': '0.42',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
