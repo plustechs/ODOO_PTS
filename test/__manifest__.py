d9bad9011bcb9870bad5194e7b8387cc57373be7
Jean Carlo <jeancarlobenites@gmail.com>
2022-08-08 18:11:26 -0500

    License

--- test/__manifest__.py
+++ test/__manifest__.py
@@ -12,10 +12,13 @@
 
     'author': "Plustechs",
     'website': "http://www.plustechs.com",
+    'license': 'LGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'application': True,
+    'installable': True,
     'category': 'Productivity',
     'version': '0.42',
 
