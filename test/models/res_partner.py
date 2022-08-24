8ffcf2227fea24c25f0b10bc5d412c73011c4b23
aquilabot <jeancarlobenites@gmail.com>
2022-08-24 12:40:29 -0500

    model

--- test/models/res_partner.py
+++ test/models/res_partner.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, api
 import json
 import requests
-
-headers = {"Content-Type": "application/json",
-           "Accept": "application/json", "Catch-Control": "no-cache"}
-url_base = "http://54.202.22.62:8080/padron-sunat/ec/gebr/20100070970"
-json_data = {}
 
 
 class ResPartner(models.Model):
@@ -15,9 +10,13 @@ class ResPartner(models.Model):
 
     @api.depends('vat', 'l10n_latam_identification_type_id')
     def _get_ruc(self):
-        _url_temp = url_base + self.vat
+        _headers = {"Content-Type": "application/json",
+                    "Accept": "application/json", "Catch-Control": "no-cache"}
+        _url_base = "http://54.202.22.62:8080/padron-sunat/ec/gebr/20100070970"
+        _json_data = {}
+        _url_temp = _url_base + self.vat
         response = requests.post(
-            _url_temp, data=json.dumps(json_data), headers=headers)
+            _url_temp, data=json.dumps(_json_data), headers=_headers)
         print(json.dumps(response.json(), indent=4, sort_keys=True))
 
     """@api.constrains('vat', 'l10n_latam_identification_type_id')
