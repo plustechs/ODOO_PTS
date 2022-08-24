a1900ffd0ebf0bb52bd4492d14d12e4244982183
aquilabot <jeancarlobenites@gmail.com>
2022-08-24 14:43:42 -0500

    self injection

--- test/models/res_partner.py
+++ test/models/res_partner.py
@@ -8,19 +8,22 @@ import requests
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.depends('vat', 'l10n_latam_identification_type_id')
+    @api.onchange('vat')
     def _get_ruc(self):
         _headers = {"Content-Type": "application/json",
                     "Accept": "application/json", "Catch-Control": "no-cache"}
         _url_base = "http://54.202.22.62:8080/padron-sunat/ec/gebr/"
         _json_data = {}
-        _url_temp = _url_base + \
-            super(ResPartner, self.env['res.partner'])._get_ruc()
+        _url_temp = _url_base + self.vat
         response = requests.post(
             _url_temp, data=json.dumps(_json_data), headers=_headers)
         print(json.dumps(response.json(), indent=4, sort_keys=True))
 
-    """@api.constrains('vat', 'l10n_latam_identification_type_id')
-    def check_vat(self):
-        with_vat = self.filtered(lambda x: x.l10n_latam_identification_type_id.is_vat)
-        return super(ResPartner, with_vat).check_vat()"""
+
+"""     @api.onchange('country_id')
+    def _onchange_country(self):
+        country = self.country_id or self.company_id.account_fiscal_country_id or self.env.company.account_fiscal_country_id
+        identification_type = self.l10n_latam_identification_type_id
+        if not identification_type or (identification_type.country_id != country):
+            self.l10n_latam_identification_type_id = self.env['l10n_latam.identification.type'].search(
+                [('country_id', '=', country.id), ('is_vat', '=', True)], limit=1) or self.env.ref('l10n_latam_base.it_vat', raise_if_not_found=False) """
