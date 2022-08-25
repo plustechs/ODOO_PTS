59eda732178cb2a881141bd3b66838ac4f4a69b2
aquilabot <jeancarlobenites@gmail.com>
2022-08-25 15:49:27 -0500

    blank

--- test/models/res_partner.py
+++ test/models/res_partner.py
@@ -7,7 +7,7 @@ import requests
 
 class ResPartner(models.Model):
     #inherit the model res.partner from l10n_latam_base
-    _inherit =  'l10n_latam_base.res_partner'
+    _inherit =  'res_partner'
 
     @api.depends('vat')
     def _get_ruc(self):
