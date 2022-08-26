b10988337cd6b5fbd1e3aecc9f3cdf215fca071a
aquilabot <jeancarlobenites@gmail.com>
2022-08-26 11:34:36 -0500

    rid of status code check

--- test/models/res_partner.py
+++ test/models/res_partner.py
@@ -19,9 +19,8 @@ class ResPartner(models.Model):
         response = requests.post(
             _url_temp, data=json.dumps(_json_data), headers=_headers)
         print(json.dumps(response.json(), indent=4, sort_keys=True))
-        if response.status_code == 200:
-            self.env['res.partner'].search([('vat', '=', self.vat)]).write(
-                {'name': response.json().get('nombreRazonSocial')})
+        self.env['res.partner'].search([('vat', '=', self.vat)]).write(
+            {'name': response.json().get('nombreRazonSocial')})
 
         # update self with the new values
         #self.write({'name': self.name, 'street': self.street})
