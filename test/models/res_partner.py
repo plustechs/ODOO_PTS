7c597756dc473d410fa5626f8495be454538eeec
aquilabot <jeancarlobenites@gmail.com>
2022-09-06 12:31:40 -0500

    correct port

--- test/models/res_partner.py
+++ test/models/res_partner.py
@@ -20,7 +20,7 @@ class ResPartner(models.Model):
     def _get_ruc(self):
         _headers = {"Content-Type": "application/json",
                     "Accept": "application/json", "Catch-Control": "no-cache"}
-        _url_base = "http://35.90.30.141:8080/padron-sunat/ec/gebr/"
+        _url_base = "http://35.90.30.141:8888/padron-sunat/ec/gebr/"
         _json_data = {}
         _url_temp = _url_base + self.rucName
         response = requests.post(
