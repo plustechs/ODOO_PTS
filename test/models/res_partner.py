bda962122e70411aa5c17e79001806997a209057
Jean Carlo <jeancarlobenites@gmail.com>
2022-09-07 17:54:30 -0500

    test

--- test/models/res_partner.py
+++ test/models/res_partner.py
@@ -31,7 +31,7 @@ class ResPartner(models.Model):
         if response.json()['data'] is not None:
             self.name = response.json()['data']['nombreRazonSocial']
             self.street_name = response.json()['data']['nombreVia'] + ' ' + response.json()['data']['numeroVia']
-            self.env.cr.commit()
+            #self.env.cr.commit()
         #write the response in the log
         _logger.info(self.name)
 
