abae055a1162900f9746e7618aebf62c2bdcc3ec
aquilabot <jeancarlobenites@gmail.com>
2022-09-13 11:51:39 -0500

    test

--- test/models/account.py
+++ test/models/account.py
@@ -27,7 +27,7 @@ class AccountMove(models.Model):
             else:
                 _logger.info('else')
                 response = ruc_fetch(self.ruc)
-                result = request_model_from_dict(json.loads(response))
+                result = request_model_from_dict(json.loads(response.json()))
                 _logger.info(result.data)
                 #self.partner_id = self.env['res.partner'].create(
                 #    {'name': 'Nuevo', 'vat': self.ruc}).id
