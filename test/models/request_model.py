cab9c333c93a68a4ae79d1301938a199463589f6
aquilabot <jeancarlobenites@gmail.com>
2022-09-14 10:11:39 -0500

    from union to support empty Data

--- test/models/request_model.py
+++ test/models/request_model.py
@@ -9,9 +9,18 @@ from typing import Any, List, TypeVar, Callable, Type, cast
 T = TypeVar("T")
 
 
-def from_str(x: Any) -> str:
-    assert isinstance(str(x), str)
-    return str(x)
+def from_none(x: Any) -> Any:
+    assert x is None
+    return x
+
+
+def from_union(fs, x):
+    for f in fs:
+        try:
+            return f(x)
+        except:
+            pass
+    assert False
 
 
 def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
@@ -45,40 +54,40 @@ class Data:
     @staticmethod
     def from_dict(obj: Any) -> 'Data':
         assert isinstance(obj, dict)
-        ruc = from_str(obj.get("ruc"))
-        nombre_razon_social = from_str(obj.get("nombreRazonSocial"))
-        estado_contribuyente = from_str(obj.get("estadoContribuyente"))
-        condicion_domicilio = from_str(obj.get("condicionDomicilio"))
-        ubigeo = from_str(obj.get("ubigeo"))
-        tipo_via = from_str(obj.get("tipoVia"))
-        nombre_via = from_str(obj.get("nombreVia"))
-        codigo_zona = from_str(obj.get("codigoZona"))
-        tipo_zona = from_str(obj.get("tipoZona"))
-        numero = from_str(obj.get("numero"))
-        interior = from_str(obj.get("interior"))
-        lote = from_str(obj.get("lote"))
-        departamento = from_str(obj.get("departamento"))
-        manzana = from_str(obj.get("manzana"))
-        kilometro = from_str(obj.get("kilometro"))
+        ruc = str(obj.get("ruc"))
+        nombre_razon_social = str(obj.get("nombreRazonSocial"))
+        estado_contribuyente = str(obj.get("estadoContribuyente"))
+        condicion_domicilio = str(obj.get("condicionDomicilio"))
+        ubigeo = str(obj.get("ubigeo"))
+        tipo_via = str(obj.get("tipoVia"))
+        nombre_via = str(obj.get("nombreVia"))
+        codigo_zona = str(obj.get("codigoZona"))
+        tipo_zona = str(obj.get("tipoZona"))
+        numero = str(obj.get("numero"))
+        interior = str(obj.get("interior"))
+        lote = str(obj.get("lote"))
+        departamento = str(obj.get("departamento"))
+        manzana = str(obj.get("manzana"))
+        kilometro = str(obj.get("kilometro"))
         return Data(ruc, nombre_razon_social, estado_contribuyente, condicion_domicilio, ubigeo, tipo_via, nombre_via, codigo_zona, tipo_zona, numero, interior, lote, departamento, manzana, kilometro)
 
     def to_dict(self) -> dict:
         result: dict = {}
-        result["ruc"] = from_str(self.ruc)
-        result["nombreRazonSocial"] = from_str(self.nombre_razon_social)
-        result["estadoContribuyente"] = from_str(self.estado_contribuyente)
-        result["condicionDomicilio"] = from_str(self.condicion_domicilio)
-        result["ubigeo"] = from_str(self.ubigeo)
-        result["tipoVia"] = from_str(self.tipo_via)
-        result["nombreVia"] = from_str(self.nombre_via)
-        result["codigoZona"] = from_str(self.codigo_zona)
-        result["tipoZona"] = from_str(self.tipo_zona)
-        result["numero"] = from_str(self.numero)
-        result["interior"] = from_str(self.interior)
-        result["lote"] = from_str(self.lote)
-        result["departamento"] = from_str(self.departamento)
-        result["manzana"] = from_str(self.manzana)
-        result["kilometro"] = from_str(self.kilometro)
+        result["ruc"] = str(self.ruc)
+        result["nombreRazonSocial"] = str(self.nombre_razon_social)
+        result["estadoContribuyente"] = str(self.estado_contribuyente)
+        result["condicionDomicilio"] = str(self.condicion_domicilio)
+        result["ubigeo"] = str(self.ubigeo)
+        result["tipoVia"] = str(self.tipo_via)
+        result["nombreVia"] = str(self.nombre_via)
+        result["codigoZona"] = str(self.codigo_zona)
+        result["tipoZona"] = str(self.tipo_zona)
+        result["numero"] = str(self.numero)
+        result["interior"] = str(self.interior)
+        result["lote"] = str(self.lote)
+        result["departamento"] = str(self.departamento)
+        result["manzana"] = str(self.manzana)
+        result["kilometro"] = str(self.kilometro)
         return result
 
 
@@ -92,18 +101,19 @@ class RequestModel:
     @staticmethod
     def from_dict(obj: Any) -> 'RequestModel':
         assert isinstance(obj, dict)
-        response_code = int(from_str(obj.get("responseCode")))
+        response_code = int(str(obj.get("responseCode")))
         error_list = from_list(lambda x: x, obj.get("errorList"))
-        data = Data.from_dict(obj.get("data"))
-        message_errors = from_str(obj.get("messageErrors"))
+        data = from_union([Data.from_dict, from_none], obj.get("data"))
+        message_errors = str(obj.get("messageErrors"))
         return RequestModel(response_code, error_list, data, message_errors)
 
     def to_dict(self) -> dict:
         result: dict = {}
-        result["responseCode"] = from_str(str(self.response_code))
+        result["responseCode"] = str(self.response_code)
         result["errorList"] = from_list(lambda x: x, self.error_list)
-        result["data"] = to_class(Data, self.data)
-        result["messageErrors"] = from_str(self.message_errors)
+        result["data"] = from_union(
+            [lambda x: to_class(Data, x), from_none], self.data)
+        result["messageErrors"] = str(self.message_errors)
         return result
 
 
