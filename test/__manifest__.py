6ddd24a8933065a1d5d3e82ed2479979c3f2720f
aquilabot <jeancarlobenites@gmail.com>
2022-09-21 15:22:46 -0500

    lic

--- test/__manifest__.py
+++ test/__manifest__.py
@@ -8,7 +8,7 @@
 
     'author': "Plustechs",
     'website': "http://www.plustechs.com",
-    'license': 'LGPL-3',
+    'license': 'OPL-1',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
